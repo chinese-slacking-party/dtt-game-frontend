f547ab987a5d6bee403635227d2385ba8f4c3e60
gracemqz <mingqian@andrew.cmu.edu>
2023-12-04 12:56:15 -0800

    alert in upload page

--- src/js/UploadPage.js
+++ src/js/UploadPage.js
@@ -1,49 +1,56 @@
 import React, { useState } from 'react';
-import '../css/UploadPage.css';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import '../css/UploadPage.css';
 import matchmelogo from '../pics/matchmelogo.svg';
 
 function UploadPage() {
   const [files, setFiles] = useState([]);
+  const navigate = useNavigate();
 
   const handleFileChange = (event, index) => {
     const newFiles = [...files];
-    newFiles[index] = event.target.files[0];
-    setFiles(newFiles);
+    if (event.target.files[0]) { // Check if file is selected
+      newFiles[index] = event.target.files[0];
+      setFiles(newFiles);
+    }
   };
 
-  const handleContinue = () => {
-    files.forEach((file, index) => {
-      const formData = new FormData();
-      formData.append('file', file);
-      const url = `/api/v1/users/:name/files/${file.name}`; // TODO: replace :name with actual name after completing login page
-      fetch(url, {
-        method: 'POST',
-        body: formData,
-      })
-        .then(response => {
-          const albumData = {
-            desc: "some description",
-            filename: file.name
-          };
-          fetch('/api/v1/album/new', {
-            method: 'POST',
-            body: JSON.stringify(albumData),
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          })
-            .then(albumResponse => {
-              // Handle album response
-            })
-            .catch(albumError => {
-              // Handle album error
-            });
-        })
-        .catch(error => {
-          // Handle upload error
+  const handleContinue = async () => {
+    console.log(files.length)
+    if (files.length < 3) {
+      alert("Please upload 3 photos to continue.");
+      return;
+    }
+
+    try {
+      await Promise.all(files.map(async (file) => {
+        const formData = new FormData();
+        formData.append('file', file);
+        const url = `/api/v1/users/:name/files/${file.name}`; // TODO: replace :name with actual name
+        await fetch(url, {
+          method: 'POST',
+          body: formData,
         });
-    });
+
+        const albumData = {
+          desc: "some description",
+          filename: file.name
+        };
+        await fetch('/api/v1/album/new', {
+          method: 'POST',
+          body: JSON.stringify(albumData),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+      }));
+
+      navigate("/ImageGame"); // Navigate after uploads are complete
+    } catch (error) {
+      console.error("Upload error:", error);
+      // Handle errors here
+    }
   };
 
   return (
@@ -61,9 +68,7 @@ function UploadPage() {
           </div>
         ))}
       </div>
-      <Link to="/ImageGame">
-        <button className="continue-button" onClick={handleContinue}>Continue</button>
-      </Link>
+      <button className="continue-button" onClick={handleContinue} >Continue</button>
     </div>
   );
 }
